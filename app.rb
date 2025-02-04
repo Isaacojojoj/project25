5421a2bf406b736767a1ba61c060fda469371662
Isaacojojoj <Isaac.ojo@elev.ga.ntig.se>
2025-02-04 16:17:19 +0100

    vecka 6
    försökte

--- app.rb
+++ app.rb
@@ -4,23 +4,42 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+def db_connection 
+    db = SQLite3::Database.new("db/chinook-crud.db")
+    db.results_as_hash = true
+    return db
+end
+
+
 get('/')  do
     slim(:start)
   end
   
 get('/inlagg') do
-    db = SQLite3::Database.new("db/chinook-crud.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM albums")
-    p result
-    slim(:"albums/index",locals:{albums:result})
+     db = db_connection
+    # result = db.execute("SELECT * FROM albums")
+    # p result
+    slim(:inlagg)
 end
 
-get('inlagg/:inlagg_id') do
+post('/inlagg/new') do
+    title = params[:title]
+    artist_id = params[:artist_id].to_i
+    p "Vi fick in datan #{title} och #{artist_id}"
+    db = SQLite3::Database.new("db/chinook-crud.db")
+    db.execute("INSERT INTO albums (Title, ArtistId) VALUES (?,?)",[title, artist_id])
+    redirect('/albums')
+  end
+
+get('/inlagg/:inlagg_id') do
     list = ["Apple", "Orange", "Banana", "Grillkorv"]
 	fruit_id = params["fruit_id"].to_i
 
 	response = list[fruit_id]
 
 	return response
+end
+
+get('/about') do
+    slim(:about)
 end
