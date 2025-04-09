e80832001e04bbae3d314bb701bcec9b1ee48ec5
Isaacojojoj <Isaac.ojo@elev.ga.ntig.se>
2025-04-09 19:35:07 +0200

    Påskinlämning
    Påskinlämning

--- app.rb
+++ app.rb
@@ -30,16 +30,24 @@ slim(:upload_image)
 end
 
 post('/image') do
-    img = params[:img]
+    img = params[:file]
     #Skapa en sträng med join "./public/uploaded_pictures/cat.png"
     path = File.join("./public/img/",params[:file][:filename])
-    
+
+    p img
+
+    p path
     #Spara bilden (skriv innehållet i tempfile till destinationen path)
     File.write(path,File.read(params[:file][:tempfile]))
     db = SQLite3::Database.new("db/databas.db")
     db.execute("INSERT INTO recept (img) VALUES (?)",[img])
     redirect('/inlagg')
    end
+   
+   post('/login') do
+   
+    redirect('/login')
+  end
    
 #    @albums_of_artist1 = db.execute(”SELECT * FROM
 #    rel_tbl INNER JOIN albums ON rel_tbl.album_id =
